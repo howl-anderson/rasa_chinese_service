d95d5ac82466e8aaf67283069a0a1e7e5241e0d9
Xiaoquan Kong <u1mail2me@gmail.com>
2020-12-21 10:12:38 +0800

    fix version issue

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rasa_chinese_service',
-    version='0.3.1',
+    version='0.0.1',
     packages=find_packages("rasa_chinese_service.*"),
     url='https://github.com/howl-anderson/rasa_chinese_service',
     license='Apache 2.0',
